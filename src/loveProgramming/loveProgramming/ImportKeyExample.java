6bdf090997121d81fe3dc425b179adb6a58caa6f
https://github.com/Rajeshwari10Bligar/WhileLoop.git <rajeshwaribaligar@gmail.com>
2019-01-08 18:42:57 +0000

    ImportKeyExample

---
+++ src/loveProgramming/loveProgramming/ImportKeyExample.java
@@ -0,0 +1,35 @@
+package loveProgramming.loveProgramming;
+
+public class ImportKeyExample {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        int []b={1,2,3,4,5,6};
+        int []c;
+        c=new int[]{1,2,3,4,5,6,7};
+        char d[];
+        d=new char[]{'a','b','c'};
+        //d=new char[]{f};try this one in home
+        //Random r = new Random();
+//        Date d ;
+        //java.util.Date da;
+        // java.sql.DriverManager driverManager
+        System.out.println("3rd index valiue="+d[2]);
+        System.out.println("length of a array is="+a[0]);
+        System.out.println("1st index value="+d[1]);
+        System.out.println("length index value="+a[1]);
+        System.out.println("2nd index valiue="+d[2]);
+        System.out.println("length of a array is="+a[1]);
+        System.out.println("4th index value="+d[3]);
+        System.out.println("length of a array is="+a[2]);
+        System.out.println("5th index value="+d[3]);
+        System.out.println("length of a array is="+a[3]);
+
+        MyClass abc = new MyClass();
+    }
+
+
+    private static class MyClass {
+    }
+}
+
+
